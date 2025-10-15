5f9cbb735178169e562992567e41f071d3215bf4
Johann Ehmann <jojo-128@gmx.de>
2025-10-16 01:17:07 +0200

    optimize vaapi for cable

--- server/encoder/ffmpeg/video_encoder_va.cpp
+++ server/encoder/ffmpeg/video_encoder_va.cpp
@@ -231,6 +231,9 @@ video_encoder_va::video_encoder_va(wivrn_vk_bundle & vk,
 			encoder_ctx->profile = AV_PROFILE_H264_CONSTRAINED_BASELINE;
 			av_dict_set(&opts, "coder", "cavlc", 0);
 			av_dict_set(&opts, "rc_mode", "CBR", 0);
+			av_dict_set(&opts, "quality", "speed", 0);
+			encoder_ctx->rc_max_rate = settings.bitrate;
+			encoder_ctx->rc_buffer_size = 2 * settings.bitrate;
 			break;
 		case video_codec::h265:
 			// bit_depth is either 8 or 10 here
